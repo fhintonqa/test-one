c57f9826842c4587ceafeb5ac19a52ac6270f42c
Felicia Hinton <feliciahinton@Felicias-Mac-mini.local>
2025-11-03 21:39:15 -0500

    Wrap up Week 1

--- Day7_loops_funct.js
+++
@@ -1,7 +0,0 @@
-function testLogin(username, password) {
-  const valid = username === "admin" && password === "test1";
-  return valid ? "✅ Success" : "❌ Error";
-}
-
-console.log(testLogin("admin", "test1")); // ✅ Success
-console.log(testLogin("john", "test"));   // ❌ Error
